9eea8df5fa63155b2b8af331eab9250d97e23205
HabtamuTesfie <95411136+HabtamuTesfie@users.noreply.github.com>
2022-03-09 13:02:10 +0300

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'bed');
+define('DB_SERVER','remotemysql.com');
+define('DB_USER','6Yj8WDRD9s');
+define('DB_PASS' ,'NmNEiNUO8F');
+define('DB_NAME', '6Yj8WDRD9s');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
