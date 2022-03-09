3d86395cdeaba779baf88231c45812c60c92e3a9
HabtamuTesfie <95411136+HabtamuTesfie@users.noreply.github.com>
2022-03-09 12:58:59 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','bed')
+$conn= new mysqli('remotemysql.com','6Yj8WDRD9s','NmNEiNUO8F','6Yj8WDRD9s')
 or die("Could not connect to mysql".mysqli_error($con));
